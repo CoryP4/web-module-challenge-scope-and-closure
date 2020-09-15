a8818c61977e0402ed79aca5f9edf10869ec2696
coryp4 <cory@burtano.com>
2020-09-14 21:23:20 -0700

    upto 3

--- index.js
+++ index.js
@@ -56,14 +56,14 @@ function counter2() {
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(max){
+function inning(range){
 
-  console.log (Math.floor(Math.random() * Math.floor(max)));
+  return (Math.floor(Math.random() * Math.floor(range)));
     /*Code Here*/
 
 }
 
- inning (3)
+ console.log (inning (3))
 
 /* Task 3: finalScore()
 
@@ -79,11 +79,15 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inning, num){
 
+  return numbers({num});
   /*Code Here*/
-
+  
 }
+
+
+
 
 /* Task 4: 
 
